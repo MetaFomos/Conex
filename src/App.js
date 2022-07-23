9b6bd6e7d878922ef1eaed3a273d1727cd6b3534
super-dev008 <super008008@outlook.com>
2022-07-23 09:01:15 +0300

    12

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { AxesHelper } from "three";
+import { FaDiscord, FaTwitter } from "react-icons/fa"
 
+
+var obj;
+let lastKnownScrollPosition = 0;
+  let ticking = false;
 function loadGLTFModel(scene, glbPath, options) {
   const { receiveShadow, castShadow } = options;
   return new Promise((resolve, reject) => {
@@ -11,7 +16,7 @@ function loadGLTFModel(scene, glbPath, options) {
     loader.load(
       glbPath,
       (gltf) => {
-        const obj = gltf.scene;
+        obj = gltf.scene;
         obj.name = "dinosaur";
         obj.position.y = 1;
         obj.position.x = -0.2;
@@ -87,6 +92,8 @@ const Dinosaur = () => {
       controls.autoRotate = false;
       controls.target = target;
       controls.enableZoom = false;
+      controls.enableRotate = false;
+      
 
       loadGLTFModel(scene, "/DState.glb", {
         receiveShadow: false,
@@ -110,7 +117,12 @@ const Dinosaur = () => {
           camera.position.z = -1.8;
           camera.lookAt(target);
         } else {
-          controls.update();
+          
+          if(window.pageYOffset>0) {
+  
+            controls.update();
+          }         
+          
         }
 
         renderer.render(scene, camera);
@@ -125,7 +137,7 @@ const Dinosaur = () => {
 
   return (
     <div
-      style={{ height: "100%", width: "100%", position: "relative" }}
+      style={{ height: "100%", width: "100%", position: "fixed" }}
       ref={refContainer}
     >
       {loading && (
@@ -138,9 +150,79 @@ const Dinosaur = () => {
 };
 
 export default function App() {
+  const [offset, setOffset] = useState(0);
+  const [step, setStep] = useState(0);
+  var lastScrollTop = 0;
+
+  window.addEventListener('scroll', function(e){
+    lastKnownScrollPosition = window.scrollY;
+
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    if (st > lastScrollTop){
+      obj.rotation.y += 0.005;
+    } else {
+      obj.rotation.y -= 0.004;
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+    
+    if (lastKnownScrollPosition < 700) {
+      setStep(0);
+    } else if (lastKnownScrollPosition >= 700 && lastKnownScrollPosition < 1300) {
+      setStep(1);
+    } else {
+      setStep(2);
+    }
+  });
+
   return (
-    <div style={{ width: window.innerWidth, height: window.innerHeight, margin: "0 auto" }}>
+    <div style={{ width: window.innerWidth, height: window.innerHeight * 3, margin: "0 auto" }}>
       <Dinosaur />
-    </div>
+      {step == 0 ? 
+      <div className="fixed z-[100] w-full h-screen">
+        <div className="relative w-full h-screen">
+          <div className="flex justify-between mt-3 mx-24 items-center">
+            <img src="/logo.png" />
+            <p className='text-white pointer text-3xl border border-white py-1 px-3'
+              >&#9776;</p>
+          </div>
+        </div>
+        <div className="relative w-full h-screen bottom-[120px]">
+          <div className="flex justify-between">
+            <div className="font-mono text-white mx-24">
+              <p className="font-[1000]">WE ARE D. STATE</p>
+              <p>Define State is the crossroads of Reality and Artificial Intelligence. AI-powered </p>
+              <p>apps, elastic tokenomics and real world assets provide value to our investors.</p>
+            </div>
+            <div className="text-white text-3xl mx-36 flex items-center">
+              <p className="border p-2 mx-1"><FaDiscord /></p>
+              <p className="border p-2"><FaTwitter /></p>
+            </div>
+          </div>
+        </div>
+      </div>
+     : ''}
+     {step == 1 ?
+     <div className="fixed z-[100] w-full h-screen">
+      <div className="flex justify-end relative w-full h-screen items-center text-white">
+        <div className="mx-24"><p className=" font-[1000] text-[40px]">CHATTER</p><br />
+        <p className="font-mono">Chatter is our AI-powered social media </p>
+        <p>aggregator tool. Get all the news, trends, </p>
+        <p>and information you need in one place.</p></div>
+      </div>
+     </div>
+     : ''
+     }
+     {step == 2 ?
+     <div className="fixed z-[100] w-full h-screen">
+      <div className="flex justify-start relative w-full h-screen items-center text-white">
+        <div className="mx-24"><p className=" font-[1000] text-[40px]">CHATTER</p><br />
+        <p className="font-mono">Chatter is our AI-powered social media </p>
+        <p>aggregator tool. Get all the news, trends, </p>
+        <p>and information you need in one place.</p></div>
+      </div>
+     </div>
+     : ''
+     }
+      </div>
   );
 }
